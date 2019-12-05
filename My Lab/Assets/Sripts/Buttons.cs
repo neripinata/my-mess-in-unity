499d17ac38e6e9399070b4ab8e46ca7d98b50d83
Neri <45918587+neripinata@users.noreply.github.com>
2019-12-04 19:11:25 -0600

    Creando un gato

---
+++ My Lab/Assets/Sripts/Buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Buttons : MonoBehaviour
+{
+    public void Apretar()
+    {
+        if (true)
+        {
+            Debug.Log("Ok");
+        }
+    }
+}
